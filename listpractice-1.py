f5a59ce7034c2f697e0049470dd45547bfd7bc41
Anzim Tasrif <109681624+anzimtasrif@users.noreply.github.com>
2025-01-28 19:28:28 +0600

    Add files via upload

---
+++ listpractice-1.py
@@ -0,0 +1,19 @@
+#Print the Second item in the fruits List.  
+
+fruits = ["apple","banana", "cherry"]
+
+print(fruits[1])
+
+
+# Change the value from "apple" to "kiwi" , in the fruits list.
+
+fruits = ["apple","banana", "cherry"] 
+
+fruits[0] = "kiwi"
+print(fruits)
+
+#Use the append method to add "orange" to the fruits list 
+
+fruits = ["apple","banana", "cherry"] 
+fruits.append("orange")
+print(fruits)
